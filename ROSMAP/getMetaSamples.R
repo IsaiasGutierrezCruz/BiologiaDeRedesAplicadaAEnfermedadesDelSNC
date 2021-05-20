0744999d22fa9b0114f54d23e16dae17df5ff46f
IsaiasGutierrez <isaiascruz@comunidad.unam.mx>
2021-05-20 13:14:06 -0500

    Get the samples to study

--- ROSMAP/getMetaSamples.R
+++
@@ -1,7 +0,0 @@
-getMetaSamples <- function(RNAseqCounts, FullMeta){
-  samples <- names(RNAseqCounts[, 3:642])
-  samples <- as.data.frame(samples)
-  MetaSamples <- FullMeta[FullMeta$specimenID %in% samples$samples, ]
-  MetaSamplesOnlyRNAseq <- MetaSamples[MetaSamples$assay == 'rnaSeq', ]
-  
-}
--- ROSMAP/main.R
+++ ROSMAP/main.R
@@ -7,13 +7,5 @@ source("mergeCounts.R")
 RNAseqCounts <- mergeCounts()
 
 # get the metadata of the necessary samples for the study
-samples <- names(RNAseqCounts[, 3:642])
-samples <- as.data.frame(samples)
-MetaSamples <- FullMeta[FullMeta$specimenID %in% samples$samples, ]
-MetaSamplesOnlyRNAseq <- MetaSamples[MetaSamples$assay == 'rnaSeq', ]
-MetaSamplesOnlyRNAseq <- MetaSamplesOnlyRNAseq[MetaSamplesOnlyRNAseq$cogdx != 1, ]
-MetaSamplesOnlyRNAseq <- MetaSamplesOnlyRNAseq[MetaSamplesOnlyRNAseq$cogdx != 6, ]
+source("getSamplesToStudy.R")
 
-
-table(MetaSamplesOnlyRNAseq$age_first_ad_dx)
-typeof(MetaSamplesOnlyRNAseq$age_first_ad_dx)
