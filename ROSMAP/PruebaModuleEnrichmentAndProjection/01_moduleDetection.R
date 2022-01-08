056d0473be1ff63cccf545f266379b59d950ba8b
Isaias Gutierrez <isaiascruz@comunidad.unam.mx>
2022-01-07 19:00:13 -0600

    Refactor begin module enrichment and projection

--- ROSMAP/PruebaModuleEnrichmentAndProjection/01_moduleDetection.R
+++ ROSMAP/PruebaModuleEnrichmentAndProjection/01_moduleDetection.R
@@ -6,23 +6,9 @@ library(data.table)
 library(tidyverse)
 #read graph
 
-g_cntrl = readRDS(file = "E:/DataROSMAPNetwork/DataOnlyWithSymbol/network_earlyOnset_with_info.rds")
-g_cases = readRDS(file = "E:/DataROSMAPNetwork/DataOnlyWithSymbol/network_lateOnset_with_info.rds")
+g_cntrl = readRDS(file = "E:/DataROSMAPNetwork/DataOnlyWithSymbol/relevanceNetworks/network_earlyOnset_with_info.rds")
+g_cases = readRDS(file = "E:/DataROSMAPNetwork/DataOnlyWithSymbol/relevanceNetworks/network_lateOnset_with_info.rds")
 
-#cases
-#E(g_cases)$weight = E(g_cases)$V2
-
-#detect modules
-modules_cases = infomap.community(graph = g_cases, nb.trials = 1)
-#assign module membership to vertices
-V(g_cases)$infomap = membership(modules_cases)
-
-write.table(x = get.data.frame(x = g_cases, "vertices"), 
-            file = "resultsWithOnlySymbl/dict_commInfomap_cases.txt", 
-            row.names = FALSE, 
-            col.names = TRUE, 
-            sep = "\t", 
-            quote = FALSE)
 
 #cntrl
 #E(g_cntrl)$weight = E(g_cntrl)$V2
@@ -34,6 +20,21 @@ V(g_cntrl)$infomap = membership(modules_cntrl)
 
 write.table(x = get.data.frame(x = g_cntrl, "vertices"), 
             file = "resultsWithOnlySymbl/dict_commInfomap_cntrl.txt", 
+            row.names = FALSE, 
+            col.names = TRUE, 
+            sep = "\t", 
+            quote = FALSE)
+
+#cases
+#E(g_cases)$weight = E(g_cases)$V2
+
+#detect modules
+modules_cases = infomap.community(graph = g_cases, nb.trials = 1)
+#assign module membership to vertices
+V(g_cases)$infomap = membership(modules_cases)
+
+write.table(x = get.data.frame(x = g_cases, "vertices"), 
+            file = "resultsWithOnlySymbl/dict_commInfomap_cases.txt", 
             row.names = FALSE, 
             col.names = TRUE, 
             sep = "\t", 
